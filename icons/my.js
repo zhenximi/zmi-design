ec2b3179a8143684c72bd1c33746475b0e850bd7
Zhenxi Mi <zhenxi.mi@gmail.com>
2016-10-14 15:57:00 -0400

    update

--- icons/my.js
+++ icons/my.js
@@ -3,7 +3,7 @@ var list = "";
 
 for (var i= 0xE001; i < 0xE090; i++) {
   var iHEX = i.toString(16).toUpperCase();
-  list = list + "<li>" + "<span class=\"icon-name\">" + iHEX +"</span>" + "<span class=\"icon-name\">&#x" + iHEX +";</span></li>"
+  list = list + "<li>" + "<span class=\"icon-name\">" + iHEX +"</span>" + "<span class=\"icon\">&#x" + iHEX +";</span></li>"
 }
 console.log (list);
 $( document ).ready(function() {
