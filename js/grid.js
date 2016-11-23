93e147723a43ebef8ac6bc1d94ac88d3a3b33df2
Zhenxi Mi <zhenxi.mi@gmail.com>
2016-11-23 10:48:18 -0500

    I would write nothing if I could

--- js/grid.js
+++ js/grid.js
@@ -1,14 +1,34 @@
 $(document).ready(function() {
     $('#sample').DataTable(
       {
-        "ajax": "data/objects.txt",
+        "scrollY":        600,
+        "scrollCollapse": true,
+        "deferRender":    true,
+        "scroller":       true,
         "paging": false,
+        "serverSide": true,
+        "ordering": false,
+        "searching": false,
+        "info": false,
+        "ajax": "data/objects.txt",
         "columns": [
         { "data": "Date" },
         { "data": "ServiceService" },
         { "data": "TypeType" },
         { "data": "UsageUsage" }
-  ]
+      ]
       }
     );
+    // $('#sample2').w2grid({
+    //     name: 'grid',
+    //     header: 'List of Names',
+    //     url: 'data/objects.txt',
+    //     columns: [
+    //         { field: 'fname', caption: 'First Name', size: '30%' },
+    //         { field: 'lname', caption: 'Last Name', size: '30%' },
+    //         { field: 'email', caption: 'Email', size: '40%' },
+    //         { field: 'sdate', caption: 'Start Date', size: '120px' }
+    //     ]
+    // });
+
 } );
